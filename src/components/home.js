41b92006a49e8059e45bc662566d2a20d7745ec1
Rachel <rslim87@gmail.com>
2017-10-02 18:59:47 -0400

    added styling to home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react'
 
 const Home = () =>
-	<div>
-		<h1>Travel Log</h1>
-		<p>Wish list of places you want to travel</p>
-		Please <a href='/signup'>sign up</a> or <a href='/login'>log in</a> to use
-	</div>
+  <Grid centered columns={4} textAlign='center' padded='vertically'>
+    <Grid.Column>
+
+				<h1>Travel Log</h1>
+				<p>Wish list of places you want to travel</p>
+				Please <a href='/signup'>sign up</a> or <a href='/login'>log in</a> to use
+
+    </Grid.Column>
+  </Grid>
 
 export default Home
